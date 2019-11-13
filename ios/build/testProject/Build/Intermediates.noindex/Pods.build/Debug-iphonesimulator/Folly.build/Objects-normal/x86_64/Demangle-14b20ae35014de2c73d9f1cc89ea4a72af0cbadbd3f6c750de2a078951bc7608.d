24ca55837ec88215c9bb57c5134f93d25319ec81
B-Robins <brodylrobins@gmail.com>
2019-11-13 17:00:27 +0000

    android initial

---
+++ ios/build/testProject/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Folly.build/Objects-normal/x86_64/Demangle-14b20ae35014de2c73d9f1cc89ea4a72af0cbadbd3f6c750de2a078951bc7608.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/brody.robins/Desktop/Projects/React-Native/appcenter-build-test/app-center-test/ios/Pods/Folly/folly/detail/Demangle.cpp \
+  /Users/brody.robins/Desktop/Projects/React-Native/appcenter-build-test/app-center-test/ios/Pods/Target\ Support\ Files/Folly/Folly-prefix.pch \
+  /Users/brody.robins/Desktop/Projects/React-Native/appcenter-build-test/app-center-test/ios/Pods/Folly/folly/detail/Demangle.h
